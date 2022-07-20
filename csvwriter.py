5a45c49db5597b0f771fcdf17db3fa6f17a5150e
Mayooonaiselol <97210788+Mayooonaiselol@users.noreply.github.com>
2022-07-20 22:34:27 +0530

    add comments

--- csvwriter.py
+++ csvwriter.py
@@ -1,8 +1,8 @@
 import csv
 
-header = ['SNo', 'Hotel_name', 'Address', 'Price', 'Rating', 'Contact']
+header = ['SNo', 'Hotel_name', 'Address', 'Price', 'Rating', 'Contact'] # Change the Column headings here.
 
-with open('delhi.csv', 'w', encoding='UTF8') as f:
+with open('delhi.csv', 'w', encoding='UTF8') as f: # Enter filename
     writer = csv.writer(f)
 
     # write the header
@@ -11,6 +11,8 @@ with open('delhi.csv', 'w', encoding='UTF8') as f:
     for i in range(1, 3):
         print(i)
         i = i + 1
+
+        # Change the input fields here
         hotel_name = input("hotel_name:")
         address = input("address:")
         price = int(input("price:"))
@@ -19,7 +21,7 @@ with open('delhi.csv', 'w', encoding='UTF8') as f:
 
         n = i - 1
 
-        data = [n, hotel_name, address, price, rating, contact]
+        data = [n, hotel_name, address, price, rating, contact] # writing in the file
 
         # write the data
         writer.writerow(data)
